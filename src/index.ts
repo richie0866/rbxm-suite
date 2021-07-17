7f47b2562ee7b0a3692b95c7fef09998ea130f81
Richard <0866playback@gmail.com>
2021-07-17 03:18:46 -0700

    Add header

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/*
+ * Author: 0866
+ * Description: Tools for loading exploit projects from Roblox model files
+ */
+
+// Main process
+
 export {};
 
 Promise.delay(2).andThen(() => print("Hello from rbxmSuite!"));
