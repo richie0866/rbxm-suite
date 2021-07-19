9edc10f293d321104fa7792a1c0e77aeb9127492
Richard <0866playback@gmail.com>
2021-07-19 03:38:04 -0700

    Create index.ts

---
+++ src/core/index.ts
@@ -0,0 +1,5 @@
+export { default as AssetDownloader, ReleaseOptions } from "./AssetDownloader";
+export { default as Project } from "./Project";
+export type { default as RbxModelBuilder } from "./RbxModelBuilder";
+export type { default as RbxScript } from "./RbxScript";
+export type { default as RbxModule } from "./RbxModule";
