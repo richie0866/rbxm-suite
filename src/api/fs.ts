94110b3e436c0af0d9d8b5a548a790ccb4a95261
Richard <0866playback@gmail.com>
2021-07-19 03:36:34 -0700

    Fix issues & add `addTrailing`

--- src/api/fs.ts
+++ src/api/fs.ts
@@ -3,7 +3,7 @@ import Object from "@rbxts/object-utils";
 type PathMap = { [K in string]: string | PathMap };
 
 /**
- * Reads the file at the give path.
+ * Reads the file at the givem path.
  */
 export const readFile = (path: string): string => readfile(path);
 
@@ -11,6 +11,16 @@ export const readFile = (path: string): string => readfile(path);
  * Returns a list of paths to the children in a directory.
  */
 export const readDir = (path: string): string[] => listfiles(path);
+
+/**
+ * Deletes the file at the given path. Throws an error if no file exists.
+ */
+// export const deleteFile = (path: string) => delfile(path);
+
+/**
+ * Deletes the directory at the given path. Throws an error if no directory exists.
+ */
+export const deleteDir = (path: string) => delfolder(path);
 
 /**
  * Checks whether the given path is a file.
@@ -25,7 +35,7 @@ export const isDir = (path: string): boolean => isfolder(path);
 /**
  * Infers whether the given path is a directory by looking for a trailing slash.
  */
-export const inferDir = (path: string): boolean => path.sub(-1) === "/";
+export const inferIsDir = (path: string): boolean => path.sub(-1) === "/";
 
 /**
  * Writes to the file at the given path.
@@ -60,9 +70,14 @@ export const createDirAll = (path: string, requireTrailingSlash = false) => {
  * });
  * ```
  */
-export const createPathTree = (pathMap: PathMap) => {
+export const createPathTree = (pathMap: PathMap, root = "") => {
 	for (const [path, data] of Object.entries(pathMap))
-		typeIs(data, "table") ? (createDirAll(path), createPathTree(data)) : writeFile(path, data);
+		if (typeIs(data, "table")) {
+			createDirAll(root + path);
+			createPathTree(data, root + addTrailing(path));
+		} else {
+			writeFile(root + path, data);
+		}
 };
 
 /**
@@ -73,4 +88,9 @@ export const trimSlashes = (path: string): string => path.gsub("^/*(.-)/*$", "%1
 /**
  * Replaces back-slashes with forward-slashes.
  */
-export const normalize = (path: string) => path.gsub("/", "\\");
+export const normalize = (path: string): string => path.gsub("/", "\\")[0] as string;
+
+/**
+ * Adds a trailing slash to the path if one doesn't already exist.
+ */
+export const addTrailing = (path: string): string => (path.sub(-1) === "/" ? path + "/" : path);
