4040354c79260ed04e91c49f32bcc1144b679446
Richard <74037129+richie0866@users.noreply.github.com>
2022-05-15 02:22:52 -0700

    Minify in JS instead

---
+++ ci/min.js
@@ -0,0 +1,15 @@
+const luamin = require("luamin");
+const path = require("path");
+const fs = require("fs");
+
+const minified = luamin.minify(fs.readFileSync(path.join(__dirname, "../src/rbxm-suite.lua"), "utf8"));
+
+const code = `local rbxmSuite = (function()
+
+	${minified}
+
+end)()
+
+return rbxmSuite`;
+
+fs.writeFileSync("rbxm-suite.lua", code);
